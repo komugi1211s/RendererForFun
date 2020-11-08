521981bbde1313f81b49e7241f9f8d3b49d2f44c
fuzzyperson@home <70929713+komugi1211s@users.noreply.github.com>
2020-11-08 15:20:41 +1000

    Build file update: uses cimgui... For NOW.

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 @echo off
 
 IF NOT EXIST ./dist mkdir dist
-set INCLUDES=
+set INCLUDES=/I "W:\CppProject\CppLib\CImgui\include"
 set COMPOPTION=/Zi /MD /Fo"dist/main_win32.obj" /Fd"dist/vc140.pdb"
 
-set LIBPATH=
-set LINKS=user32.lib shell32.lib gdi32.lib
-set LINKOPTION=/INCREMENTAL:NO /pdb:"dist/main.pdb" /out:"dist/main.exe"
+set LIBPATH=/LIBPATH:"W:\CppProject\CppLib\CImgui\lib"
+set LINKS=user32.lib shell32.lib gdi32.lib cimgui.lib
+set LINKOPTION=/INCREMENTAL:NO /pdb:"dist/main.pdb" /out:"dist/main.exe" /PROFILE
 
 cl.exe %COMPOPTION% %INCLUDES% src/main_win32.c /link %LINKOPTION% %LIBPATH% %LINKS% 
 
